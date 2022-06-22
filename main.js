e97745c296e33ae1133278fb68a2bbb59f28eb76
Hannah Jones <h.aubrey47@gmail.com>
2022-06-22 18:12:28 -0400

    Added pricing pg and started making notes in main.js pg.

---
+++ main.js
@@ -0,0 +1,7 @@
+/*
+Using JS mainly for the pricing/scheduling pg.
+The goal is to use the html/css to list the pricings & times. 
+Have JS use a clicking event listener and then do something. 
+    - Return the price the user selects back to them asking them to confirm or cancel.
+    - Then returning a new message stating 'Ok X, we'll see you on MM/DD at HH:MM. Can't wait to see you then!'
+*/
